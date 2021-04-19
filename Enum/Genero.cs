6f37a222ea8a4830466240b82ca5195d4949b8a6
MatheusVrg <matheusvergaslv078@gmail.com>
2021-04-19 09:42:37 -0300

    commit inicial

---
+++ Enum/Genero.cs
@@ -0,0 +1,16 @@
+namespace APP_cadastro_simples
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Ficcao_Cientifica = 6,
+        Musical = 7,
+        Romance = 8,
+        Suspense = 9,
+        Terror = 10
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Aplicação simples de cadastro de séries
+
+Sistema de cadastro de séries em memória (Console Application).
+
+Para a linguagem de desenvolvimento foi usado C#.
+
